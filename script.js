0f416aa046b12bfeb0059e736d6cf94981f6a667
akillika <akillika49@gmail.com>
2021-12-18 17:07:20 +0530

    reply

--- script.js
+++ script.js
@@ -31,7 +31,23 @@ $(function() {
       str += "            <img src=\"https:\/\/image.crisp.im\/avatar\/operator\/196af8cc-f6ad-4ef7-afd1-c45d5231387c\/240\/?1483361727745\">";
       str += "          <\/span>";
       str += "          <div class=\"cm-msg-text\">";
-      str += msg;
+      // console.log("Came here da!!!");
+      if(type == 'self'){
+        str += msg;
+      }
+      if(type == 'user'){
+        if(msg == "Sastra"){
+          str += "Sastra is a world class university!!";}
+        else if(msg == "Admission"){
+          str += "You are admitted";}
+        else if(msg == "Bus"){
+          str += "Yes we have it";}
+        else if(msg == "Fees"){
+          str += "Considerable Amount";}
+        else{
+          str += "I'm still growing! I need clear definition..."}
+        }
+      
       str += "          <\/div>";
       str += "        <\/div>";
       $(".chat-logs").append(str);
@@ -39,50 +55,18 @@ $(function() {
       if(type == 'self'){
        $("#chat-input").val(''); 
       }    
-      $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);    
-    }  
+      $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000); }
     
-    function generate_button_message(msg, buttons){    
-      /* Buttons should be object array 
-        [
-          {
-            name: 'Existing User',
-            value: 'existing'
-          },
-          {
-            name: 'New User',
-            value: 'new'
-          }
-        ]
-      */
-      INDEX++;
-      var btn_obj = buttons.map(function(button) {
-         return  "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\""+button.value+"\">"+button.name+"<\/a><\/li>";
-      }).join('');
-      var str="";
-      str += "<div id='cm-msg-"+INDEX+"' class=\"chat-msg user\">";
-      str += "          <span class=\"msg-avatar\">";
-      str += "            <img src=\"https:\/\/image.crisp.im\/avatar\/operator\/196af8cc-f6ad-4ef7-afd1-c45d5231387c\/240\/?1483361727745\">";
-      str += "          <\/span>";
-      str += "          <div class=\"cm-msg-text\">";
-      str += msg;
-      str += "          <\/div>";
-      str += "          <div class=\"cm-msg-button\">";
-      str += "            <ul>";   
-      str += btn_obj;
-      str += "            <\/ul>";
-      str += "          <\/div>";
-      str += "        <\/div>";
-      $(".chat-logs").append(str);
-      $("#cm-msg-"+INDEX).hide().fadeIn(300);   
-      $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight}, 1000);
-      $("#chat-input").attr("disabled", true);
-    }
+    
+    
+    
+    
     
     $(document).delegate(".chat-btn", "click", function() {
       var value = $(this).attr("chat-value");
       var name = $(this).html();
       $("#chat-input").attr("disabled", false);
+      console.log("I'm calling him!");
       generate_message(name, 'self');
     })
     
